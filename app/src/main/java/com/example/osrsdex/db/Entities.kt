d48a1667778092b138b17ed8b8116e85c75e3aed
Taliker <86399179+Taliker@users.noreply.github.com>
2023-12-18 21:53:22 -0500

    EditLoadoutActivity Works

--- app/src/main/java/com/example/osrsdex/db/Entities.kt
+++ app/src/main/java/com/example/osrsdex/db/Entities.kt
@@ -3,7 +3,6 @@ package com.example.osrsdex.db
 import android.os.Parcelable
 import androidx.room.Embedded
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
